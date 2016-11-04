b85ef5d700d5e66f009ea1873fbc9106b344d1b4
DarekG <dariusz.jan.g@gmail.com>
2016-11-04 10:25:25 +0100

    first com

---
+++ src/Server.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Server.c
+ Author      : AJ
+ Version     :
+ Copyright   : struk
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello world"); /* prints Hello world */
+	return EXIT_SUCCESS;
+}
